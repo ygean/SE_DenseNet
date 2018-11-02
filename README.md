28702925a45e3e3fb7c55cce4cf84e78ac1344e5
AllenZhou <1401499346@qq.com>
2018-11-02 19:42:07 +0800

    initialize project
    initialize project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SE_DenseNet
+
+## Introduction
 This is a DensNet  which contains a SE (Squeeze-and-Excitation Networks by Jie Hu, Li Shen and Gang Sun) module.
+
+The backbone is densenet, I just add senet into densenet as pic-1 shows, and test on my classification task, finally, se_densenet performs better than densenet on my dataset.
+
+pic-1:
+![pic-1](assets/02.png)
+
+
+I will update content and show my test result as quickly as possible.
+
+## TODO
+- [ ] test result
+- [ ] usage of my codes
---
+++ test_se_densenet.py
@@ -0,0 +1,46 @@
+import torch
+import torch.utils.model_zoo as model_zoo
+from se_densenet import se_densenet121
+# from official repo import densenet
+from torchvision.models.densenet import densenet121
+
+
+def test_se_densenet(pretrained=False):
+    X = torch.Tensor(32, 3, 224, 224)
+
+    if pretrained:
+        model = se_densenet121(pretrained=pretrained)
+
+    else:
+        model = se_densenet121(pretrained=False)
+        net_state_dict = {key: value for key, value in model_zoo.load_url("https://download.pytorch.org/models/densenet121-a639ec97.pth").items()}
+        model.load_state_dict(net_state_dict, strict=False)
+
+    # print(model)
+    if torch.cuda.is_available():
+        X = X.cuda()
+        model = model.cuda()
+    model.eval()
+    with torch.no_grad():
+        output = model(X)
+        print(output.shape)
+
+
+def test_densenet():
+    """create example tensor data for densenet, and print output variable shape"""
+    X = torch.Tensor(32, 3, 224, 224)
+
+    model = densenet121(pretrained=False)
+    
+    if torch.cuda.is_available():
+        model = model.cuda()
+        X = X.cuda()
+
+    model.eval()
+    with torch.no_grad():
+        output = model(X)
+        print(output.shape)
+
+
+if __name__ == "__main__":
+    test_se_densenet(pretrained=True)
